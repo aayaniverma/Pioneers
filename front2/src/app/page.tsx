a1e261d33b08795fa4d86f39e0ef14997ffd5e1e
Aayani Verma <aayaniverma@gmail.com>
2024-09-02 14:50:05 +0530

    modified:   src/app/page.tsx

--- front2/src/app/page.tsx
+++ front2/src/app/page.tsx
@@ -16,36 +16,18 @@ export default function Chatbot() {
 
       <main className="flex-grow p-6">
       <div className="flex justify-center mb-6">
-      <button className="btn">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor">
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-  </svg>
-  Fuel Car
-</button>
-<button className="btn">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor">
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-  </svg>
-  Button
-</button>
+      <div className="grid grid-cols-2 gap-20">
+  <button className="btn">
+    <span className="material-symbols-rounded" style={{ fontSize: '50px'}}>directions_car</span><br></br>
+    Fuel Car Dashboard Icons
+  </button>
+  <button className="btn">
+    <span className="material-symbols-rounded " style={{ fontSize: '50px'}}>electric_car</span><br></br>
+    EV Car Dashboard Icons
+  </button>
+</div>
+
+
       </div>
         <div className="max-w-2xl mx-auto bg-white rounded-lg shadow-lg p-4">
           <ChatWindow />
